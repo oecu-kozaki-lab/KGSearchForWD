fc267be61268fff3debe3da160cee53dafaeb382
Kouji Kozaki <kozaki@osakac.ac.jp>
2023-07-01 04:36:17 +0900

    Wikidata検索のサンプルの改良

---
+++ sample/wd-serach-options.js
@@ -0,0 +1,95 @@
+/* 
+ * 詳細検索用の条件設定
+ * 　・"id"の値が重複しないように注意
+ *   ・データの数は必要に応じて増減してよい． 
+ */
+search_cond = 
+[
+  {
+    "id": "cond1",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "",
+    "const": false
+  },
+  {
+    "id": "cond2",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "",
+    "const": false
+  },
+  {
+    "id": "cond3",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "",
+    "const": false
+  },
+  {
+    "id": "cond4",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "",
+    "const": false
+  },
+  {
+    "id": "cond5",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "",
+    "const": false
+  }  ,
+  {
+    "id": "cond6",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "",
+    "const": false
+  }
+];
+
+/* 
+ * 検索項目の設定
+ * 　・"id"の値が重複しないように注意
+ *   ・データの数は必要に応じて増減してよい． 
+ */
+search_prop = 
+[
+  {
+    "id": "opt1",
+    "prop": "wdt:P31",
+    "pname": "分類",
+    "optional": true
+  },
+  {
+    "id": "opt2",
+    "prop": "wdt:P279",
+    "pname": "上位クラス",
+    "optional": true
+  },
+  {
+    "id": "opt3",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  },
+  {
+    "id": "opt4",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  },
+  {
+    "id": "opt5",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  }
+];
